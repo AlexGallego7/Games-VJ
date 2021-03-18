5e336297d0a001af910200afbe20eb7e648cb977
nuriia99 <RIquy212>
2021-03-18 12:16:17 +0100

    sistema de mapa de niveles con enteros

--- 02-Bubble/TileMap.cpp
+++ 02-Bubble/TileMap.cpp
@@ -50,7 +50,8 @@ bool TileMap::loadLevel(const string &levelFile)
 	ifstream fin;
 	string line, tilesheetFile;
 	stringstream sstream;
-	char tile;
+	int tile;
+	char t;
 	
 	fin.open(levelFile.c_str());
 	if(!fin.is_open())
@@ -80,15 +81,23 @@ bool TileMap::loadLevel(const string &levelFile)
 	map = new int[mapSize.x * mapSize.y];
 	for(int j=0; j<mapSize.y; j++)
 	{
-		for(int i=0; i<mapSize.x; i++)
+
+		for (int i = 0; i < mapSize.x; i++)
 		{
-			fin.get(tile);
-			if(tile == ' ')
-				map[j*mapSize.x+i] = 0;
+			fin.get(t);
+			tile = (t - int('0'));
+			fin.get(t);
+			while (t != ' ') {
+				tile = tile * 10 + (t - int('0'));
+				fin.get(t);
+			}
+			if (tile == '0')
+				map[j * mapSize.x + i] = 0;
 			else
-				map[j*mapSize.x+i] = tile - int('0');
+				map[j * mapSize.x + i] = tile;
 		}
-		fin.get(tile);
+		fin.get(t);
+
 #ifndef _WIN32
 		fin.get(tile);
 #endif
@@ -115,7 +124,7 @@ void TileMap::prepareArrays(const glm::vec2 &minCoords, ShaderProgram &program)
 				// Non-empty tile
 				nTiles++;
 				posTile = glm::vec2(minCoords.x + i * tileSize, minCoords.y + j * tileSize);
-				texCoordTile[0] = glm::vec2(float((tile-1)%2) / tilesheetSize.x, float((tile-1)/2) / tilesheetSize.y);
+				texCoordTile[0] = glm::vec2(float((tile-1)%4) / tilesheetSize.x, float((tile-1)/4) / tilesheetSize.y);
 				texCoordTile[1] = texCoordTile[0] + tileTexSize;
 				//texCoordTile[0] += halfTexel;
 				texCoordTile[1] -= halfTexel;
