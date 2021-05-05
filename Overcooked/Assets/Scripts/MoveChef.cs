e5cbdc1477f0459f5a67844e723068882f07f776
Alex Gallego <alex_gallego99@hotmail.com>
2021-05-05 19:24:21 +0200

    Camara parecida a la del juego original

--- Overcooked/Assets/Scripts/MoveChef.cs
+++ Overcooked/Assets/Scripts/MoveChef.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class MoveChef : MonoBehaviour
 {
+    enum Dir
+    {
+        LEFT, RIGHT, UP, DOWN
+    };
+
     public float speed = 50.0f;
+    Dir direction = Dir.DOWN;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +24,32 @@ public class MoveChef : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.RightArrow))
+        {
             transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+            if (direction != Dir.LEFT)
+            {
+                switch(direction)
+                {
+                    case Dir.RIGHT:
+                        transform.Rotate(0.0f, 180, 0.0f);
+                        break;
+                    case Dir.UP:
+                        transform.Rotate(0.0f, -90, 0.0f);
+                        break;
+                    case Dir.DOWN:
+                        transform.Rotate(0.0f, 90, 0.0f);
+                        break;
+                    default:
+                        break;
+                }
+                direction = Dir.LEFT;
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
         if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(0.0f, 0.0f, -speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.UpArrow))
             transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(0.0f, 0.0f, -speed * Time.deltaTime);
     }
 }
