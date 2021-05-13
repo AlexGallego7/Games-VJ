9dbb1fc79f3382371e22af86de0c16b996240cc8
nuriia99 <nuria.anton.calle@estudiantat.upc.edu>
2021-05-13 12:41:27 +0200

    filete cortado añadido y ahora se puede volver a coger objetos bien

--- Overcooked/Assets/Scripts/ObjetoActual.cs
+++ Overcooked/Assets/Scripts/ObjetoActual.cs
@@ -10,7 +10,7 @@ public class ObjetoActual : MonoBehaviour
     public Transform handZone;
     public Transform ObjCoger;
 
-    public GameObject pan_cortado;
+    public GameObject pan_cortado, filete_cortado;
     public GameObject tomate, seta, pan, pasta, lechuga, filete, queso, pepino;
 
 
@@ -23,9 +23,16 @@ public class ObjetoActual : MonoBehaviour
     private GameObject detectar_objeto()
     {
         GameObject nuevo_objeto = null;
-        if (objetoActual.tag == "pan")
+        switch (objetoActual.tag)
         {
-            nuevo_objeto = pan_cortado;
+            case "pan":
+                nuevo_objeto = pan_cortado;
+                break;
+            case "filete":
+                nuevo_objeto = filete_cortado;
+                break;
+            default:
+                break;
         }
 
         return nuevo_objeto;
@@ -57,7 +64,51 @@ public class ObjetoActual : MonoBehaviour
                 }
             }
         }
-        if (objetoActual == null && objetoParaCoger != null)
+        //coger objeto
+        else if (objetoParaCoger != null && !objetoParaCoger.GetComponent<CogerObjeto>().cogido && objetoActual == null)
+        {
+            if (Input.GetKeyDown(KeyCode.E)) {
+                objetoActual = objetoParaCoger;
+                objetoActual.GetComponent<CogerObjeto>().cogido = true;
+                objetoActual.GetComponent<CogerObjeto>().inicializado = true;
+                if (objetoActual.GetComponent<CogerObjeto>().mesa != null) objetoActual.GetComponent<CogerObjeto>().mesa.GetComponent<ObjetoMesa>().hay_objeto = false;
+                objetoActual.transform.SetParent(handZone);
+                objetoActual.transform.position = handZone.position;
+                objetoActual.GetComponent<Rigidbody>().useGravity = false;
+                objetoActual.GetComponent<Rigidbody>().isKinematic = true;
+                
+            }
+        }
+        //soltar objeto en mesa
+        else if (objetoActual != null && objetoActual.GetComponent<CogerObjeto>().mesa != null && objetoActual.GetComponent<CogerObjeto>().mesa.GetComponent<ObjetoMesa>().hay_objeto == false)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                objetoActual.GetComponent<CogerObjeto>().cogido = false;
+                objetoActual.GetComponent<CogerObjeto>().mesa.GetComponent<ObjetoMesa>().hay_objeto = true;
+                objetoActual.transform.SetParent(ObjCoger);
+                objetoActual.transform.position = objetoActual.GetComponent<CogerObjeto>().mesa.transform.position;
+                objetoActual.transform.position -= new Vector3(0,0.5f,0);
+                objetoActual.transform.rotation = new Quaternion(0,0,0,0);
+                objetoActual.GetComponent<Rigidbody>().useGravity = false;
+                objetoActual.GetComponent<Rigidbody>().isKinematic = true;
+                objetoActual = null;
+            }
+        }
+        //soltar objeto en el suelo
+        else if (objetoActual != null && objetoActual.GetComponent<CogerObjeto>().mesa == null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                objetoActual.GetComponent<CogerObjeto>().cogido = false;
+                objetoActual.transform.SetParent(ObjCoger);
+                objetoActual.GetComponent<Rigidbody>().useGravity = true;
+                objetoActual.GetComponent<Rigidbody>().isKinematic = false;
+                objetoActual = null;
+            }
+        }
+        //coger objeto box
+        else if (objetoActual == null && objetoParaCoger != null)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
@@ -147,54 +198,10 @@ public class ObjetoActual : MonoBehaviour
                     default:
                         break;
                 }
-                
+
             }
 
         }
-        //coger objeto
-        else if (objetoParaCoger != null && !objetoParaCoger.GetComponent<CogerObjeto>().cogido && objetoActual == null)
-        {
-            if (Input.GetKeyDown(KeyCode.E)) {
-                objetoActual = objetoParaCoger;
-                objetoActual.GetComponent<CogerObjeto>().cogido = true;
-                objetoActual.GetComponent<CogerObjeto>().inicializado = true;
-                if (objetoActual.GetComponent<CogerObjeto>().mesa != null) objetoActual.GetComponent<CogerObjeto>().mesa.GetComponent<ObjetoMesa>().hay_objeto = false;
-                objetoActual.transform.SetParent(handZone);
-                objetoActual.transform.position = handZone.position;
-                objetoActual.GetComponent<Rigidbody>().useGravity = false;
-                objetoActual.GetComponent<Rigidbody>().isKinematic = true;
-                
-            }
-        }
-        //soltar objeto en mesa
-        else if (objetoActual != null && objetoActual.GetComponent<CogerObjeto>().mesa != null && objetoActual.GetComponent<CogerObjeto>().mesa.GetComponent<ObjetoMesa>().hay_objeto == false)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                objetoActual.GetComponent<CogerObjeto>().cogido = false;
-                objetoActual.GetComponent<CogerObjeto>().mesa.GetComponent<ObjetoMesa>().hay_objeto = true;
-                objetoActual.transform.SetParent(ObjCoger);
-                objetoActual.transform.position = objetoActual.GetComponent<CogerObjeto>().mesa.transform.position;
-                objetoActual.transform.position -= new Vector3(0,0.5f,0);
-                objetoActual.transform.rotation = new Quaternion(0,0,0,0);
-                objetoActual.GetComponent<Rigidbody>().useGravity = false;
-                objetoActual.GetComponent<Rigidbody>().isKinematic = true;
-                objetoActual = null;
-            }
-        }
-        //soltar objeto en el suelo
-        else if (objetoActual != null && objetoActual.GetComponent<CogerObjeto>().mesa == null)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                objetoActual.GetComponent<CogerObjeto>().cogido = false;
-                objetoActual.transform.SetParent(ObjCoger);
-                objetoActual.GetComponent<Rigidbody>().useGravity = true;
-                objetoActual.GetComponent<Rigidbody>().isKinematic = false;
-                objetoActual = null;
-            }
-        }
-
     }
 
 
