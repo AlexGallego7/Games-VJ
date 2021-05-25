e2a188210a09a66426492b6e294b972657de9eea
nuriia99 <nuria.anton.calle@estudiantat.upc.edu>
2021-05-25 16:54:37 +0200

    hacer que las recetas de ham aparezcan aleatoriamente (hasta 5 max) cada 2 segs

--- Overcooked/Assets/Scripts/ControlEscena1.cs
+++ Overcooked/Assets/Scripts/ControlEscena1.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class Game : MonoBehaviour
+public class ControlEscena1 : MonoBehaviour
 {
-
+    
 
     private int numSlots;
 
@@ -17,6 +18,8 @@ public class Game : MonoBehaviour
     public GameObject recetas;
 
     public GameObject rec_ham_sola, rec_ham_lechuga, rec_ham_queso, rec_ham_lechuga_tomate;
+
+    private bool spawn_receta = true;
 
     // Start is called before the first frame update
     void Start()
@@ -37,21 +40,48 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        if(spawn_receta) StartCoroutine("esperar10secs");
+    }
 
-        StartCoroutine("esperar10secs");
+    public GameObject consigue_receta(int num)
+    {
+        GameObject nueva_receta=null;
+        switch (num)
+        {
+            case 0:
+                nueva_receta = rec_ham_sola;
+                break;
+            case 1:
+                nueva_receta = rec_ham_lechuga;
+            break;
+            case 2:
+                nueva_receta = rec_ham_queso;
+            break;
+            case 3:
+                nueva_receta = rec_ham_lechuga_tomate;
+            break;
+            default:
+                break;
+        }
+        return nueva_receta;
     }
 
     IEnumerator esperar10secs()
     {
+        spawn_receta = false;
         yield return new WaitForSeconds(2);//cambiar a 10 en el futuro
+        GameObject nueva_receta = Instantiate(consigue_receta(Random.Range(0, 4)), new Vector3(0, 0, 0), Quaternion.identity);
+        addReceta(nueva_receta, nueva_receta.GetComponent<receta>().id, nueva_receta.GetComponent<receta>().image);
+        spawn_receta = true;
         
-        addReceta(rec_ham_lechuga_tomate, rec_ham_lechuga_tomate.GetComponent<receta>().id, rec_ham_lechuga_tomate.GetComponent<receta>().image);
     }
 
     public void addReceta(GameObject rec, int id, Sprite image)
     {
         for (int i = 0; i < numSlots; i++)
         {
+            Debug.Log(slots[i].GetComponent<slot>().empty);
             if (slots[i].GetComponent<slot>().empty)
             {
                 rec.GetComponent<receta>().activado = true;
@@ -60,13 +90,13 @@ public class Game : MonoBehaviour
                 slots[i].GetComponent<slot>().image = image;
 
                 rec.transform.position = slots[i].transform.GetChild(0).transform.position;
+                rec.transform.rotation = slots[i].transform.GetChild(0).transform.rotation;
                 slots[i].GetComponent<slot>().updateSlot();
                 rec.SetActive(true);
 
                 slots[i].GetComponent<slot>().empty = false;
-
+                break;
             }
-            return;
         }
     }
 }
