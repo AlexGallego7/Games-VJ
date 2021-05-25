f92f5da6130f144ae8716a982fe7bf5dc905a6b6
Alex Gallego <alex_gallego99@hotmail.com>
2021-05-25 23:29:59 +0200

    fix merge

--- Overcooked/Assets/Scripts/ControlEscena1.cs
+++ Overcooked/Assets/Scripts/ControlEscena1.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class ControlEscena1 : MonoBehaviour
 {
@@ -21,27 +21,52 @@ public class ControlEscena1 : MonoBehaviour
 
     private bool spawn_receta = true;
 
+    public Slider s1, s2, s3, s4, s5;
+
+    public Text tiempoTexto;
+    private float tiempo;
+
+
     // Start is called before the first frame update
     void Start()
     {
         numSlots = recetas.transform.childCount;
         slots = new GameObject[numSlots];
-        for (int i=0; i<numSlots; i++)
+        for (int i = 0; i < numSlots; i++)
         {
             slots[i] = recetas.transform.GetChild(i).gameObject;
-            if(slots[i].GetComponent<slot>().receta == null)
+            if (slots[i].GetComponent<slot>().receta == null)
             {
                 slots[i].GetComponent<slot>().empty = true;
             }
         }
         recetas.SetActive(true);
-    }
+        tiempo = 200.0f;
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
-        if(spawn_receta) StartCoroutine("esperar10secs");
+        for (int i = 0; i < numSlots; i++)
+        {
+            if (!slots[i].GetComponent<slot>().empty)
+            {
+                /*
+                if (i == 0) s1.value -= 1*Time.deltaTime;
+                else if (i == 1) s2.value -= 1 * Time.deltaTime;
+                else if (i == 2) s3.value -= 1 * Time.deltaTime;
+                else if (i == 3) s4.value -= 1 * Time.deltaTime;
+                else if (i == 4) s5.value -= 1 * Time.deltaTime;
+                */
+            }
+            else break;
+        }
+        if (spawn_receta) StartCoroutine("esperar10secs");
+        tiempo = tiempo - 1* Time.deltaTime;
+        string minutes = ((int)tiempo / 60).ToString();
+        string seconds = (tiempo % 60).ToString("f0");
+
+        tiempoTexto.text = minutes + ":" + seconds;
     }
 
     public GameObject consigue_receta(int num)
@@ -71,28 +96,32 @@ public class ControlEscena1 : MonoBehaviour
     {
         spawn_receta = false;
         yield return new WaitForSeconds(2);//cambiar a 10 en el futuro
-        GameObject nueva_receta = Instantiate(consigue_receta(Random.Range(0, 4)), new Vector3(0, 0, 0), Quaternion.identity);
-        addReceta(nueva_receta, nueva_receta.GetComponent<receta>().id, nueva_receta.GetComponent<receta>().image);
-        spawn_receta = true;
-        
+        addReceta();
     }
 
-    public void addReceta(GameObject rec, int id, Sprite image)
+    public void addReceta()
     {
         for (int i = 0; i < numSlots; i++)
         {
-            Debug.Log(slots[i].GetComponent<slot>().empty);
+           
             if (slots[i].GetComponent<slot>().empty)
             {
+                GameObject rec = Instantiate(consigue_receta(Random.Range(0, 4)), new Vector3(0, 0, 0), Quaternion.identity);
+                spawn_receta = true;
                 rec.GetComponent<receta>().activado = true;
                 slots[i].GetComponent<slot>().receta = rec;
-                slots[i].GetComponent<slot>().id = id;
-                slots[i].GetComponent<slot>().image = image;
+                slots[i].GetComponent<slot>().id = rec.GetComponent<receta>().id;
+                slots[i].GetComponent<slot>().image = rec.GetComponent<receta>().image;
 
                 rec.transform.position = slots[i].transform.GetChild(0).transform.position;
                 rec.transform.rotation = slots[i].transform.GetChild(0).transform.rotation;
                 slots[i].GetComponent<slot>().updateSlot();
                 rec.SetActive(true);
+                if (i == 0) s1.value = 100;
+                else if (i == 1) s2.value = 100;
+                else if (i == 2) s3.value = 100;
+                else if (i == 3) s4.value = 100;
+                else if (i == 4) s5.value = 100;
 
                 slots[i].GetComponent<slot>().empty = false;
                 break;
@@ -100,3 +129,4 @@ public class ControlEscena1 : MonoBehaviour
         }
     }
 }
+
--- Overcooked/Assets/Scripts/receta.cs
+++ Overcooked/Assets/Scripts/receta.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class receta : MonoBehaviour
 {
@@ -10,16 +11,18 @@ public class receta : MonoBehaviour
 
     public bool activado;
 
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
