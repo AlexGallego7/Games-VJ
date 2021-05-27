df9f460173adb1ffb6e89b1b5ddf7a37ceba9829
Alex Gallego <alex_gallego99@hotmail.com>
2021-05-27 21:55:52 +0200

    arreglar ui y no se puede pausar cuando escena intermedia

--- Overcooked/Assets/NextLevelScreen.cs
+++ Overcooked/Assets/NextLevelScreen.cs
@@ -8,21 +8,25 @@ public class NextLevelScreen : MonoBehaviour
 {
 
     public Text pointsText;
+    public bool up;
 
     public void Setup(int score)
     {
         Time.timeScale = 0f;
         gameObject.SetActive(true);
+        up = true;
         pointsText.text = score.ToString() + " points";
     }
 
     public void NextLevel ()
     {
+        up = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Menu ()
     {
+        up = false;
         SceneManager.LoadScene(0);
         FindObjectOfType<AudioManager>().Stop("Restaurant");
         FindObjectOfType<AudioManager>().Play("Theme");
